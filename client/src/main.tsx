b1f48e223810b085c22dc41de6d616c391b7d39d
eskertribe <joel.harjunheimo@gmail.com>
2025-02-09 12:50:39 +0200

    WIP

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,37 @@
-import { StrictMode } from 'react'
+import { StrictMode, createContext, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import './index.css'
 import App from './App.tsx'
 
+type ContextType = {
+  user?: { username: string, email: string } | null;
+  setUser?: (user: any) => void;
+};
+
+const UserContext = createContext<ContextType>({});
+
+import { ReactNode } from 'react';
+
+const UserProvider: any = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<{ username: string, email: string } | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider
-      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-    >
-      <App />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <UserProvider>
+        <App />
+      </UserProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
-)
+);
+
+export { UserContext };
