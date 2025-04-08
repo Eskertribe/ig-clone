900e3a686523367657dde3ee8bb8a81dcd1ce3c7
eskertribe <joel.harjunheimo@gmail.com>
2025-04-08 15:41:20 +0300

    redirect to login page if token is expired

--- client/src/AuthRouter/AuthRouter.tsx
+++ client/src/AuthRouter/AuthRouter.tsx
@@ -1,11 +1,26 @@
-import React, { FC, ReactElement, useContext } from 'react';
-import { Login } from '../Login/Login';
+import { FC, ReactElement, useContext } from 'react';
 import { AuthContext } from '../AuthContext/AuthContext';
+import { Login } from '../Login/Login';
+import { jwtDecode } from 'jwt-decode';
 
 const AuthRouter: FC<{ children: ReactElement }> = ({ children }) => {
   const { token } = useContext(AuthContext);
 
+  const isTokenExpired = (token: string): boolean => {
+    try {
+      const decoded: { exp: number } = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      return decoded.exp < currentTime;
+    } catch (error) {
+      return true;
+    }
+  };
+
   if (!token?.length) {
+    return <Login />;
+  }
+
+  if (isTokenExpired(token)) {
     return <Login />;
   }
 
