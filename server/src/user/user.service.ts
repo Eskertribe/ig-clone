f4d65fe991a9f973774af69c3d73f553682a6fbb
eskertribe <joel.harjunheimo@gmail.com>
2025-04-16 14:49:21 +0300

    Get files with the posts for userpfofile

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -3,6 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
 import { UserProfileDataDto } from './dto/user.dto';
+import { UUID } from 'crypto';
+import { join } from 'path';
+import { readFile } from 'fs/promises';
 
 @Injectable()
 export class UserService {
@@ -11,10 +14,10 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async getUserProfileDataById(userId: string): Promise<UserProfileDataDto> {
+  async getUserProfileDataById(userId: UUID): Promise<UserProfileDataDto> {
     const user = await this.userRepository.findOne({
       where: { id: userId },
-      relations: ['posts'],
+      relations: ['posts', 'posts.comments', 'posts.comments.user', 'posts.user', 'posts.file'],
       order: { posts: { createdAt: 'DESC' } },
     });
 
@@ -22,6 +25,38 @@ export class UserService {
       throw new NotFoundException('User not found');
     }
 
-    return user.toUserProfileDataDto();
+    // TODO: Refactor
+    const filePath = join(__dirname, '..', '..', 'uploads', user.profilePicture.name);
+    const imageBuffer = await readFile(filePath);
+    const profilePicture = imageBuffer.toString('base64');
+    const { posts } = user;
+
+    const postDtos = await Promise.all(
+      (posts ?? []).map(async (post) => {
+        const file = post.file;
+        const filePath = join(__dirname, '..', '..', 'uploads', file.name);
+        const imageBuffer = await readFile(filePath);
+        const base64Image = imageBuffer.toString('base64');
+
+        return post.toDto(
+          {
+            id: file.id,
+            image: `data:${file.mimeType};base64,${base64Image}`,
+          },
+          post.user.toCommentDto({
+            id: post.user.profilePicture.id,
+            image: `data:image/png;base64,${profilePicture}`,
+          }),
+        );
+      }),
+    );
+    const userDto = user.toUserProfileDataDto(postDtos);
+
+    userDto.profilePicture = {
+      id: user.profilePicture.id,
+      image: `data:image/png;base64,${profilePicture}`,
+    };
+
+    return userDto;
   }
 }
