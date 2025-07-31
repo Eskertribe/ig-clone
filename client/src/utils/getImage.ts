9def8853377ebd68ccbb7aeed112ac5ee911f8be
eskertribe <joel.harjunheimo@gmail.com>
2025-07-31 17:24:31 +0300

    Fix get image url

--- client/src/utils/getImage.ts
+++ client/src/utils/getImage.ts
@@ -5,5 +5,11 @@ export const getImageUrl = (url: string) => {
     return url;
   }
 
+  if (url.startsWith('/uploads')) {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const baseUrl = apiUrl.replace('/api/v1', '');
+    return `${baseUrl}${url}`;
+  }
+
   return url;
 };
